84b7eb050cd9828ce7d39233feb612a4a4a627ac
RC-95 <rhysconnolly@protonmail.com>
2025-07-20 17:21:29 +0200

    docs: Update constructor args for YieldAggregator verification
    - Add correct constructor arguments for YieldAggregator at 0x4DA94EAC2b1C2960Dcdcc21aEed5239bC1EAFc47
    - Include WETH (0x2D5ee574e710219a521449679A4A7f2B43f046ad) from Compound V3
    - Include LINK (0xf8Fb3713D459D7C1018BD0A49D19b4C44290EBE5) from Aave V3
    - Contract successfully verified on Sepolia Etherscan

--- constructor-args.js
+++ constructor-args.js
@@ -1,7 +1,10 @@
 // constructor-args.js
 // Constructor arguments for YieldAggregator contract verification
-// This exports an empty array for initial supported tokens
+// This exports the array of supported tokens used when deploying the contract
 
 module.exports = [
-  [] // Empty array for initial supported tokens
+  [
+    "0x2D5ee574e710219a521449679A4A7f2B43f046ad", // WETH from Compound V3
+    "0xf8Fb3713D459D7C1018BD0A49D19b4C44290EBE5"  // LINK from Aave V3
+  ]
 ]; 
